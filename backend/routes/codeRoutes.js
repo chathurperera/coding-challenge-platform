d82f4d8e5528f384451ad73abd56a517e5d3af5f
chathurperera <chathuraperera007@gmail.com>
2023-02-28 15:40:27 +0530

    root path updated

---
+++ backend/routes/codeRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { testCode, submitCode } = require('../controllers/codeController');
+
+router.post("/test", testCode);
+router.post("/submit", submitCode);
+
+module.exports = router;
